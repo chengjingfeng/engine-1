db8666aa6167b3a6ab2bd373e313b334e751b7f5
Pierre Mavro <pmavro@qovery.com>
2021-06-04 11:01:09 +0200

    fix: temporary fix for CNI

--- lib/aws/bootstrap/helm-aws-vpc-cni.j2.tf
+++ lib/aws/bootstrap/helm-aws-vpc-cni.j2.tf
@@ -34,6 +34,13 @@ EOT
   ]
 }
 
+locals {
+  aws_cni = <<CNI
+crd:
+  create: false
+CNI
+}
+
 resource "helm_release" "aws_vpc_cni" {
   name = local.aws_cni_chart_release_name
   chart = "charts/aws-vpc-cni"
